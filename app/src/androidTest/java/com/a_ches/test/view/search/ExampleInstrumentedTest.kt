7001cc84e11bdbd545fdc19037bc489ce550bf74
Learn Java <java20012@gmail.com>
2022-04-17 12:31:27 +0300

    homework 5

--- app/src/androidTest/java/com/a_ches/test/view/search/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/a_ches/test/view/search/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.a_ches.test
+package com.a_ches.test.view.search
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.a_ches.test", appContext.packageName)
+        assertEquals("com.geekbrains.tests", appContext.packageName)
     }
 }
--- app/src/main/java/com/a_ches/test/view/ViewContract.kt
+++ app/src/main/java/com/a_ches/test/view/ViewContract.kt
@@ -1,5 +1,5 @@
 package com.a_ches.test.view
 
-import com.a_ches.test.model.SearchResult
+
 
 interface ViewContract {}
